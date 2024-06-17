90be3810aa38487d80709c3b455ac6f4693ce1a2
Tuyt Mai <tranthituyetmai7430@gmail.com>
2024-06-17 20:23:56 +0700

    feat: update comment

--- src/modules/comment/entities/comment.entity.ts
+++ src/modules/comment/entities/comment.entity.ts
@@ -1,18 +1,12 @@
 import { BaseEntity } from 'src/common/entities/base.entity';
-import { Instructor } from 'src/modules/instructor/entities/instructor.entity';
 import { Lesson } from 'src/modules/lesson/entities/lesson.entity';
 import { Student } from 'src/modules/student/entities/student.entity';
-import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToMany, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'comments' })
 export class CommentEntity extends BaseEntity {
   @Column({ nullable: true, type: 'varchar' })
   content!: string;
-
-  @ManyToMany(() => Instructor, (instructor) => instructor.comments, {
-    cascade: true,
-  })
-  instructors: Instructor[];
 
   @ManyToMany(() => Student, (student) => student.comments, { cascade: true })
   students: Student[];
--- src/modules/instructor/entities/instructor.entity.ts
+++ src/modules/instructor/entities/instructor.entity.ts
@@ -1,7 +1,6 @@
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { Program } from 'src/modules/program/entities/program.entity';
-import { CommentEntity } from 'src/modules/comment/entities/comment.entity';
-import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity({ name: 'instructors' })
 export class Instructor extends BaseEntity {
@@ -25,8 +24,4 @@ export class Instructor extends BaseEntity {
 
   @OneToMany(() => Program, (program) => program.instructor, { cascade: true })
   programs: Program[];
-
-  @ManyToMany(() => CommentEntity, (comment) => comment.instructors)
-  @JoinTable()
-  comments: CommentEntity[];
 }
